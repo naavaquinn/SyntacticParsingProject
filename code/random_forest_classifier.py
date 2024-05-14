0152899f2c2aae019e1435322ff4e7f33b753d56
Mery-e <144436296+Mery-e@users.noreply.github.com>
2024-05-14 22:08:53 +0100

    Create random_forest_classifier.py

---
+++ code/random_forest_classifier.py
@@ -0,0 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
+
+# Instantiate model with 100 decision trees
+random_forest = RandomForestClassifier()
+random_forest.fit(train_X, train_Y)
+y_pred_rf = random_forest.predict(test_X)
+     
+
+evaluation_metrics("Random Forest", test_Y, y_pred_rf)
+     
