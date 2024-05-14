56568f57c850ba3153788773c23f02ea36bf2e9b
Mery-e <144436296+Mery-e@users.noreply.github.com>
2024-05-14 23:25:11 +0100

    Delete code/knn_classifier.py

--- code/knn_classifier.py
+++
@@ -1,7 +0,0 @@
-from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(train_X, train_Y)
-y_pred_knn = knn.predict(test_X)
-     
-
-evaluation_metrics("KNN", test_Y, y_pred_knn)
