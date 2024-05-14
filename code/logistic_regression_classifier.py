5002a57c22bee9efaebab15f9efba04609ca6a9c
Mery-e <144436296+Mery-e@users.noreply.github.com>
2024-05-14 22:09:29 +0100

    Create logistic_regression_classifier.py

---
+++ code/logistic_regression_classifier.py
@@ -0,0 +1,7 @@
+rom sklearn.linear_model import LogisticRegression
+logistic_regression = LogisticRegression()
+logistic_regression.fit(train_X, train_Y)
+y_pred_lr = logistic_regression.predict(test_X)
+     
+
+evaluation_metrics("Logistic Regression", test_Y, y_pred_lr)
