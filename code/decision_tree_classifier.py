cb0c13aace62e6df5ab74a7263d73259214a1192
Mery-e <144436296+Mery-e@users.noreply.github.com>
2024-05-14 23:24:58 +0100

    Delete code/decision_tree_classifier.py

--- code/decision_tree_classifier.py
+++
@@ -1,9 +0,0 @@
-from sklearn.tree import DecisionTreeClassifier
-
-# Decision Tree classifer object
-d_tree = DecisionTreeClassifier()
-d_tree.fit(train_X, train_Y)
-y_pred_d_tree = d_tree.predict(test_X)
-     
-
-evaluation_metrics("Decison Tree", test_Y, y_pred_d_tree)
